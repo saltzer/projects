ac22e9154a54c64ef7e1fbb62ef7e354af929957
Saltzer <glebanton54@gmail.com>
2021-11-09 12:40:01 +0300

    Add files via upload

---
+++ rust_projects/square_sum/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    let square_sum(vec![1, 2, 3]);
+
+    fn square_sum(vec: Vec<i32>) -> i32 {
+
+        len sum = 0;
+    
+        //assert_eq!([1, 2], &array[1..]);
+    
+        // This loop prints: 0 1 2
+        for x in array {
+            sum += x*x;
+            println!("{}", sum); 
+        }
+
+    }
+}
+//x.pow(n)
