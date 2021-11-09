c35c3d6686041a9bfd6c120bca1f5b3945f9f3ab
Saltzer <glebanton54@gmail.com>
2021-11-09 12:37:31 +0300

    Add files via upload

---
+++ rust_projects/multiplication_table_for_number/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let n: u64 = 2;
+
+    for index in 1..11 {
+        println!("{}",index * n);
+     };
+}
---
+++ rust_projects/multiplication_table_for_number/target/rls/debug/deps/multiplication_table_for_number-6c9dce20130f67d0.d
@@ -0,0 +1,5 @@
+/home/user/Documents/Projects/rust_projects/multiplication_table_for_number/target/rls/debug/deps/multiplication_table_for_number-6c9dce20130f67d0.rmeta: src/main.rs
+
+/home/user/Documents/Projects/rust_projects/multiplication_table_for_number/target/rls/debug/deps/multiplication_table_for_number-6c9dce20130f67d0.d: src/main.rs
+
+src/main.rs:
