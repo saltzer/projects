12549340731e3391d3ebd511e346b286bd5b25b4
Saltzer <glebanton54@gmail.com>
2021-12-18 05:10:59 +0300

    Update habr_bot.py

--- python_projects/habr_bot.py
+++ python_projects/habr_bot.py
@@ -9,10 +9,11 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import urllib.request
 from datetime import datetime
+import h_url
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-invite = ###########
+invite = 406200618
 
 button_help = KeyboardButton('/help')
 button_categories = KeyboardButton('/categories')
@@ -55,24 +56,24 @@ async def process_help_command(message: types.Message):
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True).add(button_help).add(button_categories)
 
-inline_button_categories_infosec = InlineKeyboardButton('Информационная безопасность',       callback_data='InfoSec')
-inline_button_categories_dev = InlineKeyboardButton('Программирование',                      callback_data='Dev')
-inline_button_categories_popscien = InlineKeyboardButton('Научно-популярное',                callback_data='PopScien')
-inline_button_categories_diy = InlineKeyboardButton('DIY',                                   callback_data='DIY')
-inline_button_categories_gadgets = InlineKeyboardButton('Гаджеты',                           callback_data='Gadgets')
-inline_button_categories_devmic = InlineKeyboardButton('Программирование микроконтроллеров', callback_data='DevMic')
-inline_button_categories_servadm = InlineKeyboardButton('Серверное администрирование',       callback_data='ServAdm')
-inline_button_categories_devops = InlineKeyboardButton('DevOps',                             callback_data='DevOps')
-inline_button_categories_net = InlineKeyboardButton('Сетевые технологии',                    callback_data='Network')
-inline_button_categories_nix = InlineKeyboardButton('*nix',                                  callback_data='Nix')
-inline_button_categories_robotics = InlineKeyboardButton('Робототехника',                    callback_data='Robot')
-inline_button_categories_sysdev = InlineKeyboardButton('Системное программирование',         callback_data='SysDev')
+b_infosec = InlineKeyboardButton('Информационная безопасность',       callback_data='InfoSec')
+b_dev = InlineKeyboardButton('Программирование',                      callback_data='Dev')
+b_popscien = InlineKeyboardButton('Научно-популярное',                callback_data='PopScien')
+b_diy = InlineKeyboardButton('DIY',                                   callback_data='DIY')
+b_gadgets = InlineKeyboardButton('Гаджеты',                           callback_data='Gadgets')
+b_devmic = InlineKeyboardButton('Программирование микроконтроллеров', callback_data='DevMic')
+b_servadm = InlineKeyboardButton('Серверное администрирование',       callback_data='ServAdm')
+b_devops = InlineKeyboardButton('DevOps',                             callback_data='DevOps')
+b_net = InlineKeyboardButton('Сетевые технологии',                    callback_data='Network')
+b_nix = InlineKeyboardButton('*nix',                                  callback_data='Nix')
+b_robotics = InlineKeyboardButton('Робототехника',                    callback_data='Robot')
+b_sysdev = InlineKeyboardButton('Системное программирование',         callback_data='SysDev')
 
-inline_kb_full = InlineKeyboardMarkup(row_width=1).add(inline_button_categories_devmic, inline_button_categories_infosec, inline_button_categories_servadm, inline_button_categories_sysdev)
-inline_kb_full.add(inline_button_categories_net, inline_button_categories_robotics)
-inline_kb_full.row(inline_button_categories_dev, inline_button_categories_popscien)
-inline_kb_full.row(inline_button_categories_gadgets, inline_button_categories_devops)
-inline_kb_full.row(inline_button_categories_diy, inline_button_categories_nix)
+inline_kb_full = InlineKeyboardMarkup(row_width=1).add(b_devmic, b_infosec, b_servadm, b_sysdev)
+inline_kb_full.add(b_net, b_robotics)
+inline_kb_full.row(b_dev, b_popscien)
+inline_kb_full.row(b_gadgets, b_devops)
+inline_kb_full.row(b_diy, b_nix)
 
 @dp.message_handler(commands=['categories'])
 async def process_categories_command(message: types.Message):
@@ -88,7 +89,7 @@ async def process_categories_command(message: types.Message):
 
 @dp.callback_query_handler(lambda c: c.data == 'InfoSec')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/infosecurity/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_infosec)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по InfoSec" + " | Время: " + str(datetime.now()) + '\n')
@@ -96,7 +97,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
         
 @dp.callback_query_handler(lambda c: c.data == 'Dev')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/programming/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_dev)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по Dev" + " | Время: " + str(datetime.now()) + '\n')
@@ -104,7 +105,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'PopScien')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/popular_science/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_popscien)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по PopScien" + " | Время: " + str(datetime.now()) + '\n')
@@ -112,7 +113,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'DIY')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/DIY/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_diy)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по DIY" + " | Время: " + str(datetime.now()) + '\n')
@@ -120,7 +121,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'Gadgets')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/gadgets/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_gadgets)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по Gadgets" + " | Время: " + str(datetime.now()) + '\n')
@@ -128,7 +129,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'DevMic')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/controllers/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_devmic)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по DevMic" + " | Время: " + str(datetime.now()) + '\n')
@@ -136,7 +137,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'ServAdm')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/s_admin/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_servadm)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по ServAdm" + " | Время: " + str(datetime.now()) + '\n')
@@ -144,7 +145,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'DevOps')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/devops/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_devops)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по DevOps" + " | Время: " + str(datetime.now()) + '\n')
@@ -152,7 +153,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'Network')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/network_technologies/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_network)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по Network" + " | Время: " + str(datetime.now()) + '\n')
@@ -160,7 +161,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'Nix')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/linux_dev/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_nix)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по Nix" + " | Время: " + str(datetime.now()) + '\n')
@@ -168,7 +169,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'Robot')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/robot/top/weekly/")
+    url = urllib.request.urlopen(h_url.url_robot)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по Robot" + " | Время: " + str(datetime.now()) + '\n')
@@ -176,7 +177,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'SysDev')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    url = urllib.request.urlopen("https://habr.com/ru/hub/system_programming/top/monthly/")
+    url = urllib.request.urlopen(h_url.url_sysdev)
     res = soup(url)
     await bot.send_message(callback_query.from_user.id, res)
     log("Выдана статья по SysDev" + " | Время: " + str(datetime.now()) + '\n')
